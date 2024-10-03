e6e6e0ea1c696cde9d909c25d99b3dc6daa4b683
Irving Enrique Vital Benitez <vital.benitez.irving.enrique@gmail.com>
2024-10-03 11:59:59 -0600

    control_usuarios

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -1,11 +1,24 @@
-const express= require('express');
+const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(404).send("Hola mundo!!!");
+app.get("/usuarios",(req,res)=>{
+    const usuarios = [
+      { id: 1,
+        nombre: "Irving",
+        apellidos: "Vital",
+        email: "irvingenrique164@gmail.com",
+      },
+
+      { id: 2,
+        nombre: "Sergio",
+        apellidos: "Cervantes",
+        email: "irvingenrique164@gmail.com",
+      },
+    ];
+    res.status(200).send(usuarios);
 });
 
 app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000");
+    console.log("Servidor cooriendo en http://localhost:3000")
 });
