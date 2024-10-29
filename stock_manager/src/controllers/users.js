53ffba258ff433445f07b74abab2304fc92c392a
Irving Enrique Vital Benitez <vital.benitez.irving.enrique@gmail.com>
2024-10-29 11:31:52 -0600

    node_modules

--- stock_manager/src/controllers/users.js
+++ stock_manager/src/controllers/users.js
@@ -26,6 +26,60 @@ const getById= (req = request, res= response) => {
     } 
 
     res.send(user);
-}
-
-module.exports={getAll, getById}; 
+}// Agregar un nuevo usuario
+const addUser = (req = request, res = response) => {
+    const { name } = req.body;
+    if (!name) {
+      res.status(400).send('Name is required');
+      return;
+    }
+    const newUser = { id: users.length + 1, name };
+    users.push(newUser);
+    res.status(201).send(newUser);
+  };
+  
+  // Actualizar un usuario existente
+  const updateUser = (req = request, res = response) => {
+    const { id } = req.params;
+    const { name } = req.body;
+  
+    if (isNaN(id)) {
+      res.status(400).send('Invalid ID');
+      return;
+    }
+  
+    const user = users.find((user) => user.id === +id);
+    if (!user) {
+      res.status(404).send('User not found');
+      return;
+    }
+  
+    if (!name) {
+      res.status(400).send('Name is required');
+      return;
+    }
+  
+    user.name = name;
+    res.send(user);
+  };
+  
+  // Eliminar un usuario
+  const deleteUser = (req = request, res = response) => {
+    const { id } = req.params;
+  
+    if (isNaN(id)) {
+      res.status(400).send('Invalid ID');
+      return;
+    }
+  
+    const index = users.findIndex((user) => user.id === +id);
+    if (index === -1) {
+      res.status(404).send('User not found');
+      return;
+    }
+  
+    users.splice(index, 1);
+    res.status(204).send(); // 204 No Content indica éxito sin contenido adicional
+  };
+  
+  module.exports = { getAll, getById, addUser, updateUser, deleteUser };
