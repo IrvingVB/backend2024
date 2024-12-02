93ab470bfcd2f93cbc9a00f83c71ad8d7f767c03
Irving Enrique Vital Benitez <vital.benitez.irving.enrique@gmail.com>
2024-12-02 12:06:30 -0600

    Agregar pokemon_game

--- pokemon_game/src/server.js
+++ pokemon_game/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const usersRoutes = require('./routes/users');
 
-class Server{
+class Server {
     constructor(){
         this.app = express();
         this.port = 3000;
@@ -14,15 +15,14 @@ class Server{
     }
 
     routes(){
-
+        this.app.use('/users', usersRoutes);
     }
 
     start(){
         this.app.listen(this.port, () => {
-            console.log(`Server is running on port ${this.port}`);
+        console.log(`Server is running on port ${this.port}`);
         });
     }
-
 }
 
-module.exports = {Server};
+module.exports = Server;
