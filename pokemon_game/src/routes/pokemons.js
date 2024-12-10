90dae37723cc23d34f8622b7fede392f46c6742f
Irving Enrique Vital Benitez <vital.benitez.irving.enrique@gmail.com>
2024-12-10 09:57:08 -0600

    Agregar pokemon_game

--- pokemon_game/src/routes/pokemons.js
+++ pokemon_game/src/routes/pokemons.js
@@ -1,10 +1,15 @@
-const {Routes, Router} = require('express');
-const {getAllPokemons, getPokemonById, get3RandomPokemons, createPokemon, updatePokemon, deletePokemon} = require('../controllers/pokemons'); 
+const {Router} = require('express');
+const { getAllPokemons, 
+    getPokemonById, 
+    get3RandomPokemons, 
+    createPokemon, 
+    updatePokemon, 
+    deletePokemon } = require('../controllers/pokemons');
 
 const routes = Router();
 
 routes.get('/', getAllPokemons);
-routes.get('/', getPokemonById);
+routes.get('/:id', getPokemonById);
 routes.get('/play', get3RandomPokemons);
 routes.post('/', createPokemon);
 routes.put('/:id', updatePokemon);
